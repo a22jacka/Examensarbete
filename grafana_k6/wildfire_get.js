84ecec34ac48a91d1b38c8107845d668c59826c4
a22jacka <a22jacka@his.se>
2025-03-18 12:03:33 +0100

    get-test functioning

---
+++ grafana_k6/wildfire_get.js
@@ -0,0 +1,23 @@
+// run with: k6 run wildfire_get.js --console-output=filename.csv
+
+import http from 'k6/http';
+
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+
+export const options = {
+    vus: 100,
+    //iterations: 1000,
+    duration: '2m'
+};
+
+export default function () {
+    const testId = uuidv4();
+    const startTime = Date.now();
+
+    const response = http.get("http://localhost:8080/wildfires");
+
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+    console.log(`${testId}, ${startTime}, ${endTime}, ${duration}, ${response.status}, ${response.timings.duration}`)
+}
