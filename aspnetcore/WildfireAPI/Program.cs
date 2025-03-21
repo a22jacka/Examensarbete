52302fcef7729d540efc8cf2abe6c434bf72500c
a22jacka <a22jacka@his.se>
2025-03-21 15:23:30 +0100

    better async handling

--- aspnetcore/WildfireAPI/Program.cs
+++ aspnetcore/WildfireAPI/Program.cs
@@ -15,16 +15,22 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/wildfires", async (int? limit, int? offset) =>
 {
     using var connection = new MySqlConnection(MYSQL_CONNECTION_STRING);
-    await connection.OpenAsync();
-
-    string query = "SELECT * FROM WildfireEntry";
+    using var command = connection.CreateCommand();
+    command.CommandText = "SELECT * FROM WildfireEntry";
     // adds limit and offset if both are present, ignores otherwise
     if (limit is not null && offset is not null)
-        query += $" LIMIT {limit} OFFSET {offset}";
-    using var command = new MySqlCommand(query + ";", connection);
+    {
+        command.CommandText += @" LIMIT @limit OFFSET @offset";
+        command.Parameters.AddWithValue("@limit", limit);
+        command.Parameters.AddWithValue("@offset", offset);
+    }
+    command.CommandText += ";";
 
+    await connection.OpenAsync();
     using var reader = await command.ExecuteReaderAsync();
-    return Results.Ok(WildfireEntry.ContructEntriesFromReader(reader));
+    List<WildfireEntry> entries = await WildfireEntry.ContructEntriesFromReader(reader);
+    await connection.CloseAsync();
+    return Results.Ok(entries);
 });
 
 app.MapPost("/wildfires/addentry", async (Stream requestBody) =>
